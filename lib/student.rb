e3c417e26b66aa1abd75756be483976fee12e3ec
github username <github email address>
2020-02-08 09:36:58 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,18 +2,95 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = Student.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+          SELECT * FROM students;
+          SQL
+
+    student_rows = DB[:conn].execute(sql)
+
+    student_rows.map{|row|
+      Student.new_from_db(row)
+    }
+  end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+        SELECT * FROM students
+        WHERE grade = 9;
+        SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+        SELECT * FROM students
+        WHERE grade < 12;
+    SQL
+
+    student_rows = DB[:conn].execute(sql)
+
+    student_rows.map{|row|
+      Student.new_from_db(row)
+    }
+  end
+
+  def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 10
+      LIMIT ?;
+      SQL
+
+      DB[:conn].execute(sql, num).map{|row| Student.new_from_db(row)}
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+        SELECT * FROM students
+        WHERE grade = 10
+        LIMIT 1;
+        SQL
+
+      DB[:conn].execute(sql).map{|row| Student.new_from_db(row)}[0]
+  end
+
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+          SELECT * FROM students
+          WHERE grade = ?
+          SQL
+
+    DB[:conn].execute(sql, grade).map{|row| Student.new_from_db(row)}
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+          SELECT * FROM students
+          WHERE name = ?
+          LIMIT 1;
+    SQL
+
+    student_row = DB[:conn].execute(sql, name).flatten
+    Student.new_from_db(student_row)
   end
+
+
+
+
+
   
   def save
     sql = <<-SQL
